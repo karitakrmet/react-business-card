28f2d993d69c94aa79740cef8ded8f358297cd89
karitakrmet <karita.karmet@gmail.com>
2020-01-25 21:18:43 +0200

    Updates

---
+++ precache-manifest.66ee268e396aff0020967de5d68d8c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f999f537d4e57c9045073d8c30f3031f",
+    "url": "/react-business-card/index.html"
+  },
+  {
+    "revision": "ac78ec70dba22bb56c1c",
+    "url": "/react-business-card/static/css/main.764f5869.chunk.css"
+  },
+  {
+    "revision": "4ecfbe1e62b60c6bcd94",
+    "url": "/react-business-card/static/js/2.07f29921.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-business-card/static/js/2.07f29921.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac78ec70dba22bb56c1c",
+    "url": "/react-business-card/static/js/main.93186204.chunk.js"
+  },
+  {
+    "revision": "85f6c0ab3b81359e829a",
+    "url": "/react-business-card/static/js/runtime-main.4beb8aaf.js"
+  }
+]);
